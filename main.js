365508f06f667486d3bf1c3b874e006668aa4704
Blake Netzeband <blake.netzeband@gmail.com>
2017-07-10 22:06:58 -0600

    sending filtered data to row class

--- main.js
+++ main.js
@@ -1,58 +1,112 @@
 import Expo from 'expo';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, Modal } from 'react-native';
+import { StyleSheet, Text, TextInput, View, ListView, TouchableOpacity, TouchableHighlight, Modal } from 'react-native';
+
+import Row from './components/row';
 
 export default class App extends Component {
-  state = {
-    isModalVis : false,
-    contactPerm : null,
+  constructor(props){
+    super(props)
+    this.handlePress = this.handlePress.bind(this)
+    this.renderModal = this.renderModal.bind(this)
+    this.getContacts = this.getContacts.bind(this)
+    this.requestContactPerm = this.requestContactPerm.bind(this)
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      isModalVis : false,
+      contactPerm : null,
+      searchitem : null,
+      contactData: null,
+      ds: ds,
+    }
   }
 
   async requestContactPerm(){
     const permission = await Expo.Permissions.askAsync(Expo.Permissions.CONTACTS);
     this.setState({contactPerm: permission.status})
+    this.getContacts()
   }
 
-  handlePress(){
-    if (this.state.contactPerm !== 'granted')
+  async handlePress(){
+    if (this.state.contactPerm !== 'granted') {
       alert("We're not the NSA! We need permission to access your contacts. Namaste!")
-    else {
-      contacts = Expo.Contacts.getContactsAsync({
-        fields: [ Expo.Contacts.PHONE_NUMBERS ],
-      });
-      this.setModalVis(!this.state.isModalVis)
+      this.requestContactPerm()
+    } else {
+      this.renderModal();
+      this.setModalVis(!this.state.isModalVis);
     }
   }
+
+  async getContacts(){
+    const contactsFull = await Expo.Contacts.getContactsAsync({
+      fields: [ Expo.Contacts.PHONE_NUMBERS ]
+    });
+    contacts = contactsFull.data.filter(function(contact){
+      return contact.phoneNumbers.length > 0;
+    }).map(function(contact){
+      return {
+        first: contact.firstName.toLowerCase().charAt(0).toUpperCase(),
+        last: contacts.lastName.toLowerCase().charAt(0).toUpperCase(), 
+      }
+    })
+    this.setState({ contactData: this.state.ds.cloneWithRows(contacts) })
+  }
+
+  componentWillMount(){
+    if (this.state.contactPerm !== 'granted') this.requestContactPerm()
+    else if (this.state.contactData === null) this.getContacts()
+  }
+
 
   setModalVis(vis){
     this.setState({isModalVis: vis})
   }
 
-  render(){
-    if (this.state.contactPerm !== 'granted') this.requestContactPerm()
+  renderModal(){
+    if (this.state.contactData === null) {
+      return (
+        <View style={styles.modalError}>
+          <Text>🤔 Having some trubs getting your contacts.</Text>
+          <Text>Let's try this again. Namaste 🙏</Text>
+          <TouchableHighlight onPress={() => { this.setModalVis(!this.state.isModalVis) }} >
+            <Text>Close</Text>
+          </TouchableHighlight>
+        </View>
+      )
+    } else return (
+      <View style={styles.modalSuccess}>
+        <TextInput onChangeText={(searchitem) => this.setState({searchitem})}
+                   value={this.state.searchitem}
+                   style={styles.searchBar}
+                   />
+        <ListView dataSource={this.state.contactData}
+                  renderRow={(data) => <Row {...data} />}
+                  renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+                  />
+        <TouchableHighlight onPress={() => { this.setModalVis(!this.state.isModalVis) }} >
+          <Text>Close</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
 
+  render(){
     return (
       <View style={styles.container}>
-      <Modal
-        animationType={"slide"}
-        transparent={false}
-        visible={this.state.isModalVis}
-        >
-       <View style={{marginTop: 22}}>
-        <View>
-          <Text>Hello World!</Text>
 
-          <TouchableHighlight onPress={() => { this.setModalVis(!this.state.isModalVis) }} >
-            <Text>Hide Modal</Text>
-          </TouchableHighlight>
+        <Modal animationType={"slide"}
+               transparent={false}
+               visible={this.state.isModalVis} >
+          { this.renderModal() }
+        </Modal>
 
-        </View>
-       </View>
-      </Modal>
-        <TouchableOpacity onPress={this.handlePress.bind(this)}>
+        <TouchableOpacity onPress={this.handlePress}>
           <Text style={styles.icon}>🙏</Text>
           <Text style={styles.title}>Send a Namaste</Text>
         </TouchableOpacity>
+
       </View>
     );
   }
@@ -68,11 +122,34 @@ const styles = StyleSheet.create({
   icon: {
     fontSize: 100,
   },
-
   title: {
     fontSize: 27,
   },
-
+  modalError: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    padding: 30,
+  },
+  modalSuccess: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 22,
+    height: '90%',
+  },
+  searchBar: {
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '100%',
+    height: 30,
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  },
 });
 
 Expo.registerRootComponent(App);
